af3c92cef0465cb287b6a6351f4277d62b75da28
FlorenciaRoque <froquejusto@gmail.com>
2026-01-20 22:50:53 -0300

    corrida con mejores hiperparametros obtenidos por optuna

--- src/rl_algorithms/q_learning_agent.py
+++ src/rl_algorithms/q_learning_agent.py
@@ -46,6 +46,11 @@ class QLearningAgent:
         self.gamma = hparams.get("gamma", 0.99) if hparams else 0.99
         self.epsilon = hparams.get("epsilon", 0.01) if hparams else 0.01
 
+        # hiperparametros hallados por optuna (hardcodeados!)
+        self.alpha = 0.000147884125071172
+        self.gamma = 0.825768976412576
+        self.epsilon = 0.0027324920916384277
+
         # Inicializar Q en el agente
         n_states = inner.observation_space.n
         n_actions = inner.action_space.n
